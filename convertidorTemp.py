f20cc70b4d50c463c5636f67a01fec1d92c9be6f
Jefferson Rivera <jeff@coink.com>
2020-08-08 10:05:28 -0500

    add to kelvin

--- convertidorTemp.py
+++ convertidorTemp.py
@@ -1,3 +1,5 @@
 celsius = float(input("Ingrese el valor de la temperatura en celsius: "))
 celsiusfahrenheit = (celsius * 9/5) + 32
+kelvin  = (celsius + 273.15)
 print('%.2f°  Celsius Equivale a: %0.2f° Fahrenheit' %(celsius, fahrenheit))
+print('%.2f°  Celsius Equivale a: %0.2f° kelvin' %(celsius, kelvin))
