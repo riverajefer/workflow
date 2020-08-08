2ef13f46c19052be8b2ce45f5c550ef704f22d00
Jefferson Rivera <jeff@coink.com>
2020-08-08 10:03:15 -0500

    fix converter

--- convertidorTemp.py
+++ convertidorTemp.py
@@ -1,3 +1,3 @@
 celsius = float(input("Ingrese el valor de la temperatura en celsius: "))
-celsiusfahrenheit = (celsius * 9/5) + 33
+celsiusfahrenheit = (celsius * 9/5) + 32
 print('%.2f°  Celsius Equivale a: %0.2f° Fahrenheit' %(celsius, fahrenheit))
